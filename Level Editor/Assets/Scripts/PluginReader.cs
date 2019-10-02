7b8c93ae535402d4399949681b067d7cfb6d9b0b
Adam Bilyea <adam.bilyea@ontariotechu.net>
2019-10-02 17:00:35 -0400

    In the midst of trying to send arrays to C++

--- Level Editor/Assets/Scripts/PluginReader.cs
+++ Level Editor/Assets/Scripts/PluginReader.cs
@@ -8,53 +8,48 @@ public class PluginReader : MonoBehaviour
      const string DLL_NAME = "SIMPLEPLUGIN";
     [DllImport(DLL_NAME)]
     private static extern int TestFunction();
-    //[DllImport(DLL_NAME)]
-   // private static extern int SaveFunction(float x, float y, float z);
-    //[DllImport(DLL_NAME)]
-    //private static extern Vector3 LoadFunction();
+    [DllImport(DLL_NAME)]
+    private static extern int SaveFunction(float x, float y, float z, int count);
+    [DllImport(DLL_NAME)]
+    private static extern Vector3 LoadFunction();
 
-    //public GameObject Player;
-    //private Rigidbody rb;
 
-    //float PosX, PosY, PosZ;
+    private Factory myScript;
+    public GameObject Box;
+    private Rigidbody rb;
+
+    float PosX, PosY, PosZ;
     
-    //void Start()
-    //{
-    //    rb = Player.GetComponent<Rigidbody>();
-    //}
-
-    void Update()
+    void Start()
     {
-       if (Input.GetKeyDown(KeyCode.T))
-       {
-            Debug.Log(TestFunction());
-       }
-
-       //       if (Input.GetKeyDown(KeyCode.T))
-       //{
-       //     PosX = rb.GetComponent<Rigidbody>().position.x;
-       //     PosY = rb.GetComponent<Rigidbody>().position.y;
-       //     PosZ = rb.GetComponent<Rigidbody>().position.z;
-//
-       //     Debug.Log("Save to file test");
-       //     SaveFunction(PosX, PosY, PosZ);
-       //     Debug.Log(TestFunction());
-           // Debug.Log(PosX + " : " + PosY + " : " + PosZ + " : ");
-      // }
-
-       // if (Input.GetKeyDown(KeyCode.L))
-       //{
-       //   Vector3 loc = LoadFunction();
-
-      //  Debug.Log(loc.x);
-       // Debug.Log(loc.y);
-       // Debug.Log(loc.z);
-
-       // Player.GetComponent<Rigidbody>().position = new Vector3(loc.x, loc.y, loc.z);
-
-       // rb.GetComponent<Rigidbody>().position = new Vector3(PosX, PosY, PosZ);
-
-            //Debug.Log(LoadFunction());
-       //}
+        rb = Box.GetComponent<Rigidbody>();
     }
+
+     public void SaveToDLL()
+     {
+          
+            PosX = rb.GetComponent<Rigidbody>().position.x;
+            PosY = rb.GetComponent<Rigidbody>().position.y;
+            PosZ = rb.GetComponent<Rigidbody>().position.z;
+
+            Debug.Log("Save to file test");
+            SaveFunction(PosX, PosY, PosZ, myScript.ObjectCount);
+            Debug.Log(SaveFunction(PosX, PosY, PosZ, myScript.ObjectCount));
+     }
+
+     public void LoadFromDLL()
+     {
+          Vector3 loc = LoadFunction();
+
+          Debug.Log(loc.x);
+          Debug.Log(loc.y);
+          Debug.Log(loc.z);
+
+          Box.GetComponent<Rigidbody>().position = new Vector3(loc.x, loc.y, loc.z);
+
+          rb.GetComponent<Rigidbody>().position = new Vector3(PosX, PosY, PosZ);
+
+          Debug.Log(LoadFunction());
+          Debug.Log("Loaded");
+     }
 }
